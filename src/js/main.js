ef5b20afa68a5d63ae8929d41c76224bdefccb8c
Deleroth <simge.toksoz@gmail.com>
2017-05-05 11:10:44 +0200

    friday second

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,3 @@
 console.log('Hello world!');
 
-var headerHeight = null;
 
-$(document).ready(function() {
-	console.log('ready');
-	headerHeight = $('header').height();
-	console.log (headerHeight);
-});
-
-$(window).resize(function() {
-	console.log('resize');
-});
-
-$(window).scroll(function() {
-	console.log('scroll');
-});
