31f306ee7abf8fc4b824fb61348b21e1ee531164
Deleroth <simge.toksoz@gmail.com>
2017-05-09 12:23:38 +0200

    tueesday

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,9 @@ console.log('Hello world!');
  +	*/ 
  	console.log (scroll);
 	var offsetAmount = -($(window).scrollTop() * 0.5);
+	console.log (offsetAmount);
 	$('section.backgroundimage').css({
 		'background-position-y': offsetAmount
+
 	});
 	});
