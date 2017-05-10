da3af638f7be46e202074f976e4f74c03519a73d
Deleroth <simge.toksoz@gmail.com>
2017-05-10 12:09:32 +0200

    wednesday

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 console.log('Hello world!');
 
-
+$(document).ready(function() {
 
   $(window).scroll(function() {
   	/*
@@ -11,6 +11,23 @@ console.log('Hello world!');
 	console.log (offsetAmount);
 	$('section.backgroundimage').css({
 		'background-position-y': offsetAmount
+		});
+	});
 
+
+	/*
+		Gallery
+	*/
+
+	var images = ['img/schoolmedia.png', 'img/superheroes.png', 'img/wedgeit.png', 'img/lightshadow.png'];
+	var counter = 0;
+
+	$('.gallery img').click(function() {
+		counter++;
+		if (counter === images.length) {
+			counter = 0;
+		}
+		$(this).attr('src', images[counter]);
 	});
-	});
+
+});
